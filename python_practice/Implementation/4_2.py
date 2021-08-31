f807cf191661223ce9a954d78a71521e607d3755
YOOHYOJEONG <ryuhyojung@naver.com>
2021-09-01 08:19:07 +0900

    Algorithm practice

--- python_practice/Implementation/4_2.py
+++ python_practice/Implementation/4_2.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 27 07:53:31 2021
+Created on Fri Aug 31 07:53:31 2021
 
 @author: user
 """
 
+#이것이 코딩테스트다 - 나동빈
+#구현 chapter 04
+# 4-2 시각
+
+
+#정수가 입력되면 모든 시각 중 3이 포함되는 경우의 수 구하기
+
+#3중 for문으로 시각을 1씩 증가시키면서 3이 포함되어 있는지 확인하면 됨.
+
+h = int(input())
+
+count = 0
+for i in range(h +1) :
+    for j in range(60) :
+        for k in range(60) :
+            if '3' in str(i) + str(j) + str(k) :
+                count += 1
+                
+print(count)
--- python_practice/Implementation/4_3.py
+++ python_practice/Implementation/4_3.py
@@ -1,21 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 31 08:40:21 2021
 
+@author: user
+"""
 
 #이것이 코딩테스트다 - 나동빈
-#구현 chapter 04
-# 4-2 시각
+# 구현 chapter 04
+# 4-3 왕실 나이트
 
 
-#정수가 입력되면 모든 시각 중 3이 포함되는 경우의 수 구하기
+#체스판 이동 경우의 수
+#나이트는 2가지 경로로 움직임
+#수평으로 두칸 이동 후 수직으로 한칸 이동
+#숮딕으로 두칸 이동 후 수평으로 한칸 이동
 
-#3중 for문으로 시각을 1씩 증가시키면서 3이 포함되어 있는지 확인하면 됨.
 
-h = int(input())
+#현재 나이트 위치 입력받기
+input_data = input()
 
-count = 0
-for i in range(h +1) :
-    for j in range(60) :
-        for k in range(60) :
-            if '3' in str(i) + str(j) + str(k) :
-                count += 1
-                
-print(count)
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+
+#나이트가 이동할 수 있는 방향 8가지 지정
+steps = [(-2,-1), (-1, -2), (1,-2), (2, -1), (2,1), (1,2), (-1,2), (-2,1)]
+
+#8가지 방향에 대해 각 위치로 이동 가능한지 확인
+result = 0 
+for step in steps :
+    #원하는 이동 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    
+    #이동 가능 하면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >=1 and next_column <= 8 :
+        result += 1
+
+print(result)
