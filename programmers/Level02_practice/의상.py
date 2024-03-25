372b2b9c767ce9144369b1f38d02f67e01371c11
YOOHYOJEONG <ryuhyojung@naver.com>
2024-03-25 23:51:04 +0900

    24.03.25 programmers algorithm practice

---
+++ programmers/Level02_practice/의상.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 18 21:05:47 2021
+
+@author: user
+"""
+
+#프로그래머스
+#level02
+#해시
+#의상
+
+def solution(clothes):
+    answer = 1
+    
+    closet = {}
+    for Kind, Type in clothes:
+        if Type not in closet.keys():
+            closet[Type] = [Kind]
+        else:
+            closet[Type] += [Kind]
+    
+    for key, value in closet.items():
+        answer *= (len(value)+1)
+    
+    return answer -1
